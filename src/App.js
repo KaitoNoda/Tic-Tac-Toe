2b6986210d3be2ff9751255dee2d063fe6d58758
kaitonoda <noda@students.cs.ubc.ca>
2023-05-08 10:52:15 -0700

    mapを使用して過去の動きを画面に表示 #17

--- src/App.js
+++ src/App.js
@@ -63,8 +63,22 @@ export default function Game() {
   }
 
   function jumpTo(nextMove) {
-
+    // TODO
   }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
 
   return (
     <div className="game">
@@ -72,7 +86,7 @@ export default function Game() {
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-        <ol>{/*TODO*/}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
