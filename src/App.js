264338645060e0b40010b6c83950fc0fc83eee61
kaitonoda <noda@students.cs.ubc.ca>
2023-05-08 10:49:58 -0700

    jumpTo関数の追加 #17

--- src/App.js
+++ src/App.js
@@ -62,6 +62,10 @@ export default function Game() {
     setXIsNext(!xIsNext);
   }
 
+  function jumpTo(nextMove) {
+
+  }
+
   return (
     <div className="game">
       <div className="game-board">
